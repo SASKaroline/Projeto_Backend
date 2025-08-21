92647ea3432b19914627e9c2186ef9fc673ea31e
SASKaroline <sasilvakarol@gmail.com>
2025-08-20 21:29:56 -0300

    Criando primeiro projeto NODE

---
+++ praticas/01-primeiro-projeto/index.js
@@ -0,0 +1,21 @@
+console.log("Rodando meu primeiro projeto em NODE")
+
+let nome = "Karol"
+
+let sobrenome = "Silva"
+
+console.log(nome + " " + sobrenome)
+
+let idade = 19
+
+console.log("A idade da " + nome + " é " + idade)
+
+let exercicios = 0.5
+
+let trabalho = 1.5
+
+let prova = 4.5
+
+let notaCalculada = exercicios + trabalho + prova
+
+console.log("NOTA A1: " + notaCalculada)
